4b6eedcb3b73a4495ed15c21f7fc25bfb94c9abe
ADOwen <92840021+ADOwen@users.noreply.github.com>
2022-11-13 15:26:00 -0500

    set up logic for server to serve index.html to client

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('../client/'))
+require('./routes/htmlRoutes.js')(app)
 
 app.listen(PORT, function() {
   console.log(`Now listening on port: ${PORT}`);
