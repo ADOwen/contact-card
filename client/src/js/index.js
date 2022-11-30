4a4dc294a39115cd61344ebab2117e42b28a6351
ADOwen <92840021+ADOwen@users.noreply.github.com>
2022-11-30 14:55:39 -0500

    added indexDB and ability to install application natively

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,15 +1,16 @@
 import './form';
 import './submit';
-
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
-
 import '../css/index.css';
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initDb, getDb, postDb } from './database';
 
-import { initDb, initdb } from './database';
+const installBtn = document.getElementById('installBtn');
+
+
 
 window.addEventListener('load', function () {
   initDb();
@@ -17,3 +18,24 @@ window.addEventListener('load', function () {
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
 });
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = 'visible';
+
+  installBtn.addEventListener('click', () => {
+    event.prompt();
+    installBtn.setAttribute('disabled', true);
+    installBtn.textContent = 'Installed!';
+    });
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event);
+});
+
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+  navigator.serviceWorker.register('./service-worker.js');
+})};
